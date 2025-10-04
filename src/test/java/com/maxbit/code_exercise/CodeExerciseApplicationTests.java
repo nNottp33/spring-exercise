447e97c787dd6b986fc6d2182e266e8bf850c07b
nnottp <nnattaponp.dev@gmail.com>
2025-10-04 15:46:09 +0700

    Initial commit

---
+++ src/test/java/com/maxbit/code_exercise/CodeExerciseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.maxbit.code_exercise;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CodeExerciseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
